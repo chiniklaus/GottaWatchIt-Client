5a1fce1c1ca2fc43ff12ce5d323687aa8eaad1b6
qiandayu17 <qian.d@husky.neu.edu>
2020-06-10 03:24:20 -0400

    styled recommendations

--- src/components/profile/Recommendation.js
+++ src/components/profile/Recommendation.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MDBBtn } from 'mdbreact';
+import { MDBBtn, MDBIcon } from 'mdbreact';
 import MovieService from "../../services/MovieService";
 
 class Recommendation extends React.Component {
@@ -23,23 +23,31 @@ class Recommendation extends React.Component {
 
     render() {
         return (
-            <div className="card m-5" style={{backgroundColor: 'rgba(255,255,255,0.3)', width: 800}}>
+            <div className="card m-3" style={{backgroundColor: 'rgba(255,255,255,0.3)', maxWidth: 1000, minWidth: 800}}>
                         <div className="row no-gutters">
-                            <div>
+                            <div className="col-3">
                                 <img 
                                     style={{height: 300}}
                                     className="img-fluid p-2" 
                                     src={this.state.movie.poster}
-                                    onClick={() => this.props.navigate(this.props.re.imdbid)}
                                 />
                             </div>
+                            <div className="col-8 mt-3 pl-3">
+                                <h3>{this.state.movie.name}</h3>
+                                <h5>from:{' ' + this.props.re.fromUsername}</h5>
+                                <h1 className="card-title font-weight-bold pt-5">{this.props.re.title}</h1>
+                            </div>
+                        </div>
+                        <div className="row no-gutters">
                             <div>
                                 <div className="card-body">
-                                    <h3 className="card-title font-weight-bold">{this.props.re.fromUsername + ': ' + this.props.re.title}</h3>
-                                    <h5 className="ccard-subtitle pt-1">
-                                        <stonrg>{this.props.re.words}</stonrg>
+                                    <h5 className="card-subtitle pt-1 mb-5 font-italic container">
+                                        <MDBIcon icon="quote-left" className="pr-2"/>
+                                        {this.props.re.words}
+                                        <MDBIcon icon="quote-right" className="pl-2"/>
                                     </h5>
-                                    <MDBBtn className="text-center" onClick={() => this.props.delete(this.props.re.id)}>Remove</MDBBtn>
+                                    <MDBBtn className="text-center mt-2" onClick={() => this.props.delete(this.props.re.id)}>Remove</MDBBtn>
+                                    <MDBBtn className="text-center mt-2" color="primary" onClick={() => this.props.navigate(this.props.re.imdbid)}>Check it out!</MDBBtn>
                                 </div>
                             </div>
                         </div>
